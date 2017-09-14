040c6bae6ed494438e38ad6fcd0ed59f0d383c77
GregorSondermeier <gregor.sondermeier.github@gmail.com>
2017-09-14 14:02:38 +0200

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,21 @@
 $(window).load(function() {
 	loadContacts();
+	registerFilters();
 });
 
+/**
+ * @type {Array<Contact>}
+ */
 var contacts;
+
+/**
+ * @type {{gender: string, district: string}}
+ */
+var searchcriteria = {
+	gender: null,
+	availableDistricts: [],
+	district: null
+};
 
 function loadContacts() {
 	$.ajax({
@@ -12,7 +25,63 @@ function loadContacts() {
 		contacts = data.map(function(iContact) {
 			return new Contact(iContact);
 		});
-		console.debug(contacts);
+		sanitizeDiscrictFilter(contacts);
+		filterContacts(contacts);
 	});
 }
 
+function filterContacts(contacts) {
+
+	var filteredContacts = [],
+		remainingContacts = [];
+
+	contacts.forEach(function(contact) {
+		console.debug('searchcriteria.gender:', searchcriteria.gender);
+		console.debug('contact.gender:', contact.gender);
+		if (!searchcriteria.gender || searchcriteria.gender == contact.gender) {
+			filteredContacts.push(contact);
+		} else {
+			remainingContacts.push(contact);
+		}
+	});
+
+	addToTable('filtered', filteredContacts);
+	addToTable('remaining', remainingContacts);
+}
+
+function addToTable(type, contacts) {
+	var tbodyElem;
+
+	if (type === 'filtered') {
+		tbodyElem = $('.gs-filtered-results > table > tbody');
+	} else if (type === 'remaining') {
+		tbodyElem = $('.gs-remaining-results > table > tbody');
+	}
+
+	tbodyElem.empty();
+
+	contacts.forEach(function(contact) {
+		tbodyElem.append($(
+			'<tr>' +
+			'<td>' + [contact.firstName, contact.lastName].join(' ') + '</td>' +
+			'<td><a href="tel:' + contact.phone + '">' + contact.phone + '</a></td>' +
+			'<td>' + contact.city + '</td>' +
+			'<td>' + contact.district + '</td>',
+			'</tr>'
+		));
+	});
+}
+
+function registerFilters() {
+	$('.gs-filter-gender select').change(function() {
+		searchcriteria.gender = $(this).val() || null;
+		filterContacts(contacts);
+	});
+}
+
+function sanitizeDiscrictFilter(contacts) {
+	// @stub
+	// analyze contacts and build an array of available disctricts, put it into
+	// searchcriteria.availableDistricts
+	// and sanitize the options
+}
